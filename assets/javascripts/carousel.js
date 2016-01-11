b4128fa0bc989b8811d2ae1f7ce05ee7507ab171
Richie Khoo <evolve2k@gmail.com>
2016-01-12 09:35:36 +1030

    Added testimonial carousel

--- assets/javascripts/carousel.js
+++ assets/javascripts/carousel.js
@@ -32,10 +32,10 @@
 
   Carousel.VERSION  = '3.3.6'
 
-  Carousel.TRANSITION_DURATION = 600
+  Carousel.TRANSITION_DURATION = 100000 // default 600
 
   Carousel.DEFAULTS = {
-    interval: 5000,
+    interval: 10000, //default 5000
     pause: 'hover',
     wrap: true,
     keyboard: true
